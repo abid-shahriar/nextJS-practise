75b5a717ce2c416ee001eac9c55ff473a1a16e1f
abid7 <abidshahriar7@gmail.com>
2021-01-29 17:22:37 +0600

    starting my first next app

---
+++ server/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import userRoutes from './routes/user.js';
+
+dotenv.config();
+
+const URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/user', userRoutes);
+
+mongoose
+	.connect(URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	})
+	.then(() => {
+		console.log('connected to db');
+		app.listen(PORT, () => {
+			console.log('app running');
+		});
+	});
---
+++ server/controllers/user.js
@@ -0,0 +1,112 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const secret = process.env.JWT_SECRET;
+
+import userModel from '../models/userModel.js';
+
+// user Sign In
+export const signIn = async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		const oldUser = await userModel.findOne({ email });
+
+		if (!oldUser) return res.status(404).json({ message: 'no user found with this email' });
+
+		const isPassCorrect = await bcrypt.compare(password, oldUser.password);
+
+		if (!isPassCorrect) return res.status(400).json({ message: 'invalid Credentials' });
+
+		const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
+			expiresIn: '1w'
+		});
+
+		const userData = { email: oldUser.email, firstName: oldUser.firstName, lastName: oldUser.lastName, id: oldUser._id };
+
+		res.status(200).json({ ...userData, token });
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+// user Sign Up
+export const signUp = async (req, res) => {
+	const { firstName, lastName, email, password } = req.body;
+
+	try {
+		const oldUser = await userModel.findOne({ email });
+
+		if (oldUser) return res.status(400).json({ message: 'User Already Exist' });
+
+		const hashedPass = await bcrypt.hash(password, 12);
+
+		const newUser = await userModel.create({ firstName, lastName, email, password: hashedPass });
+
+		const token = jwt.sign({ email: newUser.email, id: newUser._id }, secret, {
+			expiresIn: '1w'
+		});
+
+		const userData = { email: newUser.email, firstName: newUser.firstName, lastName: newUser.lastName, id: newUser._id };
+
+		res.status(201).json({ ...userData, token });
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+// Update User info
+export const updateUser = async (req, res) => {
+	if (!req.userID) return res.status(400).json({ message: 'Unauthorized User' });
+
+	const userID = req.userID;
+
+	const { firstName, lastName, email } = req.body;
+
+	try {
+		const otherUserWithEmail = await userModel.findOne({ email });
+
+		if (otherUserWithEmail) {
+			const oldUser = await userModel.findOne({ _id: userID });
+
+			if (!oldUser) return res.status(400).json({ message: 'Unauthorized User' });
+
+			if (otherUserWithEmail.email === oldUser.email) {
+				const updateUserData = { firstName, lastName, email };
+
+				const updateUser = await userModel.findByIdAndUpdate(userID, updateUserData, { new: true });
+
+				const token = jwt.sign({ email: updateUser.email, id: updateUser._id }, secret, {
+					expiresIn: '1w'
+				});
+
+				const userData = { email: updateUser.email, firstName: updateUser.firstName, lastName: updateUser.lastName, id: updateUser._id };
+
+				res.status(201).json({ ...userData, token, message: 'Profile Update Success' });
+			} else {
+				res.status(400).json({ message: 'the email is already taken' });
+			}
+		} else {
+			const oldUser = await userModel.findOne({ _id: userID });
+
+			if (!oldUser) return res.status(400).json({ message: 'Unauthorized User' });
+
+			const updateUserData = { firstName, lastName, email };
+
+			const updateUser = await userModel.findByIdAndUpdate(userID, updateUserData, { new: true });
+
+			const token = jwt.sign({ email: updateUser.email, id: updateUser._id }, secret, {
+				expiresIn: '1w'
+			});
+
+			const userData = { email: updateUser.email, firstName: updateUser.firstName, lastName: updateUser.lastName, id: updateUser._id };
+
+			res.status(201).json({ ...userData, token, message: 'Profile Update Success' });
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
